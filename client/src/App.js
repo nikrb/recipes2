07f9c54203c7b868cd69b9624ef9b6ad9c034050
nikrb <nik_rb@yahoo.com>
2017-05-12 00:43:46 +0100

    add font sizing buttons to menu bar

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,14 @@ import Recipe from './Recipe';
 import './App.css';
 
 class App extends Component {
+  state = {
+    font_size : 10
+  };
   componentWillMount = () => {
+    const size = localStorage.getItem( 'font_size');
+    if( size !== null){
+      this.setState( { font_size: size});
+    }
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', function() {
         navigator.serviceWorker.register('service-worker.js').then(function(registration) {
@@ -22,9 +29,23 @@ class App extends Component {
       console.log('service worker is not supported');
     }
   };
+  smallerFont = () => {
+    localStorage.setItem( 'font_size', this.state.font_size-1);
+    this.setState( { font_size: this.state.font_size - 1});
+  };
+  largerFont = () => {
+    localStorage.setItem( 'font_size', this.state.font_size+1);
+    this.setState( { font_size: this.state.font_size + 1});
+  };
   render = () => {
     const app_style = {
-      fontSize: "1em"
+      fontSize: `${this.state.font_size/10}em`
+    };
+    const large_font = {
+      fontSize: ".9em"
+    }
+    const small_font = {
+      fontSize: ".6em"
     };
     return (
       <div className="App" style={app_style}>
@@ -34,6 +55,10 @@ class App extends Component {
               <ul>
                 <li><Link to="/">Home</Link></li>
               </ul>
+              <div>
+                <button type="button" style={small_font} onClick={this.smallerFont} >A</button>
+                <button type="button" style={large_font} onClick={this.largerFont} >A</button>
+              </div>
             </div>
             <hr/>
             <h2>Mum's Recipes</h2>
